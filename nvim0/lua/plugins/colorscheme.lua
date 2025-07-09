e5f82ecdd5ecf670758f3f8099166dd507ab5cea
hardmando <totonn777@icloud.com>
2025-07-08 19:59:43 -0500

    nvim config

--- nvim0/lua/plugins/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'dgox16/oldworld.nvim',
-    event = 'VimEnter',
-    opts = { variant = 'oled' },
-    config = function()
-      require('oldworld').setup { variant = 'oled' }
-      vim.cmd.colorscheme 'oldworld'
-    end,
-}
--- nvim0/lua/plugins/guess-indent.lua
+++
@@ -1,3 +0,0 @@
-return {
-	'NMAC/guess-indent.nvim'
-}
